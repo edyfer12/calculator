470e3614a58398ef8d1688bc2b9743dbe170b354
Edin <edyfer2@live.com.au>
2024-02-15 18:17:03 +0100

    Declare variables

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 //Declare a divide function that will divide two numbers returned
 
 //Declare a variable called firstNumber that will be used to store the first number
+let firstNumber;
 //Declare a variable called secondNumber that will be used to store the second number
+let secondNumber;
 //Declare a variable called operator that will be used to store the operator(+,-,/,*)
+let operator;
 
 //Create a function that will be used to perform a mathematical operation between two numbers
     //If a user inputs two numbers and clicks a + button, call the add function
