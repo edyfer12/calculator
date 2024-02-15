2a510daa8cfea70fab20ed213d31f6176f646410
Edin <edyfer2@live.com.au>
2024-02-15 17:08:20 +0100

    Add comments

---
+++ script.js
@@ -0,0 +1,3 @@
+//Declare a variable called firstNumber that will be used to store the first number
+//Declare a variable called secondNumber that will be used to store the second number
+//Declare a variable called operator that will be used to store the operator(+,-,/,*)
