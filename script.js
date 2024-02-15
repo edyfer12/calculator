55926c6d4672da4660da1b328561b2f68f87ab65
Edin <edyfer2@live.com.au>
2024-02-15 18:16:08 +0100

    Add comments

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//Declare an add function that will add two numbers returned
+//Declare a subtract function that will subtract two numbers returned
+//Declare a multiply function that will multiply two numbers returned
+//Declare a divide function that will divide two numbers returned
+
 //Declare a variable called firstNumber that will be used to store the first number
 //Declare a variable called secondNumber that will be used to store the second number
 //Declare a variable called operator that will be used to store the operator(+,-,/,*)
+
+//Create a function that will be used to perform a mathematical operation between two numbers
+    //If a user inputs two numbers and clicks a + button, call the add function
+    //If a user inputs two numbers and clicks a - button, call the subtract function
+    //If a user inputs two numbers and clicks a * button, call the multiply function
+    //If a user inputs two numbers and clicks a / button, call the divide function
