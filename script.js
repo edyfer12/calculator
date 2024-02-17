0841a4d4ed5f300e478dfd9ba77c87ca802e330e
Edin <edyfer2@live.com.au>
2024-02-17 16:02:58 +0100

    Add comment

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ function display(){
     const secondNumber = [];
     //Create variable where it stores the second operand 
     let operandSecond;
+    //Create variable that captures the reference to the equals sign button
     //For each number button in the numbers nodelist,
     numbers.forEach((number) => {
         //Create variable where it indicates to user if the operator is clicked or not
