5d8cb8e8a407975a3b08cc869bc641ed6064ed87
Edin <edyfer2@live.com.au>
2024-02-17 19:19:00 +0100

    Remove variable and comment

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ function divide(firstNumber, secondNumber){
 let firstNumber;
 //Declare a variable called secondNumber that will be used to store the second number
 let secondNumber;
-//Declare a variable called operator that will be used to store the operator(+,-,/,*)
-let operator;
 
 //Create a function that will be used to perform a mathematical operation between two numbers
 function operate(firstNumber,secondNumber, operator){
