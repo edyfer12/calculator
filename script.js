767ebb34e10a63906fcac1e0fae4ef210db8af49
Edin <edyfer2@live.com.au>
2024-02-17 10:23:28 +0100

    Push the number in an array

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function display(){
         //If a user clicks on a number button, display the number on a text box
         number.addEventListener('click', () => {
             //Populate the numbers in an array that stores numbers
+            arrNumbers.push(number);
             output.value = number.textContent;
         });
     });
