0423e02b1c8ba3e6bc81ea62c88b575056c797d2
Edin <edyfer2@live.com.au>
2024-02-17 15:38:39 +0100

    Create variable and set value to false

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ function display(){
     //For each number button in the numbers nodelist,
     numbers.forEach((number) => {
         //Create variable where it indicates to user if the operator is clicked or not
+        let operatorClicked = false;
         //If a user clicks on a number button, 
         number.addEventListener('click', () => {
             //Populate the numbers in a first number array that stores digits that occurs before operation
