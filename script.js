742121acfbebe7d2e7a34ec56dd70cf35ade3a64
Edin <edyfer2@live.com.au>
2024-02-19 11:09:18 +0100

    Add comments

--- script.js
+++ script.js
@@ -67,6 +67,10 @@ function display(){
         digit.addEventListener('click', () => {
             //Push each digit into the digits array
             digits.push(digit.textContent);
+            //If first number exists,
+            //Join all digits from array converted to number into secondNumber variable
+            //Display the value of second number into the textbox
+            //If first number does not exist,
             //Join all digits from array converted to number into firstNumber variable
             firstNumber = Number(digits.join(''));
             //Display the value of first number into the textbox
@@ -83,6 +87,7 @@ function display(){
         operator.addEventListener('click', () => {
             //Remove all digits in the digits array
             digits.splice(0,digits.length);
+            //Store in the operator button's text content into the operatorName variable
         });
     });
     
