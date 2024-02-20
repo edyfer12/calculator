1c30aeed29e7445b4a777e8ec6db83a35e8e070c
Edin <edyfer2@live.com.au>
2024-02-20 19:13:22 +0100

    Remove the comments

--- script.js
+++ script.js
@@ -305,10 +305,6 @@ function display(){
                 operatorName = operator.textContent;
                 //Push the operatorName variable into the arithmetic array   
                 arithmetic.push(operatorName);
-                //Display the result value between the operation of two numbers
-                //If operatorName is +, 
-                    //call an operate function with three values passed firstNumber, operatorName, secondNumber
-                    //Display the result on the textbox
             } 
         });
     });
