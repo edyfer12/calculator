92ab96bffdf790c9c95432d79a17e2b1a28f104b
Edin <edyfer2@live.com.au>
2024-02-21 09:27:15 +0100

    Add comment

--- script.js
+++ script.js
@@ -299,6 +299,7 @@ function display(){
             else if(arithmetic[arithmetic.length - 2] === firstNumber && 
                 arithmetic[arithmetic.length - 1] === operatorName){
                 //Push secondNumber into the arithmetic array 
+                //If th secondNumber is not undefined,
                 arithmetic.push(secondNumber);
                 console.log('arithmetic 2nd = ', arithmetic);
                 //If item of index 0 is firstNumber, item of last index is secondNumber and length of arithmetic array is 3 
