3a260a11c1031655491d77b3438670282a7eabb6
Edin <edyfer2@live.com.au>
2024-02-23 20:06:37 +0100

    Add comments

--- script.js
+++ script.js
@@ -352,6 +352,8 @@ function display(){
         else if(arithmetic[arithmetic.length - 1] === '=' && arithmetic[arithmetic.length - 2] === firstNumber &&
         firstNumber == undefined && arithmetic[arithmetic.length - 3] === operatorName && 
         arithmetic[arithmetic.length - 4] === secondNumber && secondNumber !== undefined){
+            //Pop the last item in the arithmetic array that is an equal sign
+            //Pop the next last item in the arithmetic array that is an undefined first number
         }
         //Otherwise, empty the arithmetic array, first number and second number and return the output value as Invalid Input: Cannot click Operator Before equal 
         else{
